7ac7438981efdcd131a9043362af29f23cf34703
victor-praise <61267951+victor-praise@users.noreply.github.com>
2025-09-21 22:10:29 -0400

    fixed tailwind bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { SignedIn } from "@clerk/nextjs";
 import { ArrowRight } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function LandingPage() {
